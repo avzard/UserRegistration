2da0022b8a5a66ebb9ebfba3e7396cf462562ec8
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-06-29 10:21:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        public static string REGEX_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome_To_UserRegistration_RegeEx");
+            FirstNameVal();
+        }
+        public static void FirstNameVal()
+        {
+            Regex rg = new Regex(REGEX_PATTERN);
+            Console.Write("Enter First Name: ");
+            string first_name = Console.ReadLine();
+            bool validate = rg.IsMatch(first_name);
+            if (validate)
+                Console.WriteLine("Name is Valid : " + first_name);
+            else
+                Console.WriteLine("Invalid FirstName");
+
+        }
+    }
+}
